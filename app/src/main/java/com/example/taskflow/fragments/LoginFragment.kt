d245d9945bf1f8ab46df86466a2cefc912263fab
Kavin1701 <skavin1701@gmail.com>
2024-04-02 23:11:12 +0530

    2 apr

---
+++ app/src/main/java/com/example/taskflow/fragments/LoginFragment.kt
@@ -0,0 +1,64 @@
+package com.example.taskflow.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.taskflow.R
+import com.example.taskflow.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+
+
+class LoginFragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var navControl: NavController
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init(view)
+        registerEvents()
+    }
+
+    private fun init(view: View){
+        navControl = Navigation.findNavController(view)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    private fun registerEvents(){
+
+        binding.loginBt.setOnClickListener{
+            val email = binding.emailEt.text.toString().trim()
+            val pass = binding.passEt.text.toString().trim()
+
+            if(email.isNotEmpty() && pass.isNotEmpty()){
+                auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener({
+                    if(it.isSuccessful){
+                        Toast.makeText(context, "Login Successfully", Toast.LENGTH_SHORT).show()
+                        navControl.navigate(R.id.action_loginFragment_to_homeFragment)
+                    }
+                    else{
+                        Toast.makeText(context, it.exception?.message, Toast.LENGTH_SHORT).show()
+                    }
+                })
+            }
+            else{
+                Toast.makeText(context, "Empty fields are not allowed", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
